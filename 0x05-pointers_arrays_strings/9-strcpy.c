5bace4cc762ace9861050eba9ee24cc5084c888d
billernest128 <hernestbill@gmail.com>
2023-09-18 12:14:57 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * char 8_stypy - function that copies the string pointed to by src
+ *@dest: destination
+ *@src: source
+ *Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int inc = 0;
+
+	while (*(src + inc) != '\0')
+	{
+		*(dest + inc) = *(src + inc);
+		inc++;
+	}
+	*(dest + inc) = '\0';
+
+	return (dest);
+}
