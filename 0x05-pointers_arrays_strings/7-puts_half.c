cb1f88f288df5c277042524ee5e0e98378f2b9c8
billernest128 <hernestbill@gmail.com>
2023-09-18 13:16:28 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,25 @@
 #include "main.h"
+
 /**
- *puts_half - puts half of a string
- *@str: string
- *Return: void
+ * _atoi - changes a string to an int
+ * @s: the string to be changed
+ *
+ * Return: the converted int
  */
 
-int _str(char *s)
+int _atoi(char *s)
 {
-	int count, inc;
+	int i = 1;
 
-	inc = 0;
+	unsigned int num = 0;
 
-	for (count = 0; s[count] != '\0'; count++)
-		inc++;
-
-	return (inc);
+	do      {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++)
+	return (num * i);
 }
