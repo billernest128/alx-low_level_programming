40d32c4f768998ad7274537b751eff88fcbb30f4
billernest128 <hernestbill@gmail.com>
2023-09-28 17:37:02 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,45 +1,30 @@
-#include "main.h"
+#include <stdio.h>
+#include <math.h>
 
-/**
- * is_prime_number - Checks if a number is prime.
- * @n: The number to check for primality.
- *
- * Return: 1 if n is prime, 0 otherwise.
- */
+int is_prime_recursive(int n, int divisor)
+{
+    if (n <= 1)
+        return 0;
+    if (n <= 3 || (n % 2 != 0 && n % 3 != 0 && divisor * divisor > n))
+        return 1;
+    if (n % 2 == 0 || n % 3 == 0 || n % divisor == 0 || n % (divisor + 2) == 0)
+        return 0;
+
+    return is_prime_recursive(n, divisor + 6);
+}
+
 int is_prime_number(int n)
 {
-	int divisor;
+    return is_prime_recursive(n, 5);
+}
 
-	if (n <= 1)
-	{
-	/* 0 and 1 are not prime */
-	return (0);
-	}
+int main(void)
+{
+    printf("is_prime_number(1): %s\n", is_prime_number(1) ? "SUCCESS" : "FAILURE");
+    printf("is_prime_number(103): %s\n", is_prime_number(103) ? "SUCCESS" : "FAILURE");
+    printf("is_prime_number(-7): %s\n", is_prime_number(-7) ? "SUCCESS" : "FAILURE");
+    printf("is_prime_number(104729): %s\n", is_prime_number(104729) ? "SUCCESS" : "FAILURE");
 
-	if (n <= 3)
-	{
-	/* 2 and 3 are prime */
-	return (1);
-	}
-
-	if (n % 2 == 0 || n % 3 == 0)
-	{
-	/* Divisible by 2 or 3, not prime */
-	return (0);
-	}
-
-	divisor = 5;
-	while (divisor * divisor <= n)
-	{
-	if (n % divisor == 0 || n % (divisor + 2) == 0)
-	{
-	/* Divisible by divisor or divisor + 2, not prime */
-	return (0);
-	}
-	divisor += 6;
-	}
-
-	/* o divisors found, n is prime */
-	return (1);
+    return 0;
 }
 
