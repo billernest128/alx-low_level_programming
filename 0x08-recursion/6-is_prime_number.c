e9b868f001bcd5d90dc1035e824cd6249d00de20
billernest128 <hernestbill@gmail.com>
2023-09-28 18:02:10 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-int is_prime_recursive(int n, int divisor)
+int is_prime_recursive(int n, int divisor);
 {
-    if (n <= 1)
-        return 0;
-    if (n <= 3 || (n % 2 != 0 && n % 3 != 0 && divisor * divisor > n))
-        return 1;
-    if (n % 2 == 0 || n % 3 == 0 || n % divisor == 0 || n % (divisor + 2) == 0)
-        return 0;
+	if (n <= 1)
+	return (0);
+	if (n <= 3 || (n % 2 != 0 && n % 3 != 0 && divisor * divisor > n))
+	return (1);
+	if (n % 2 == 0 || n % 3 == 0 || n % divisor == 0 || n % (divisor + 2) == 0)
+	return (0);
 
-    return is_prime_recursive(n, divisor + 6);
+	return (is_prime_recursive(n, divisor + 6));
 }
 
 int is_prime_number(int n)
 {
-    return is_prime_recursive(n, 5);
+	return (is_prime_recursive(n, 5));
 }
 
