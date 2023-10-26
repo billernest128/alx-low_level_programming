0c35bf811e0d71fa61dae181f4cb28565560e0ff
billernest128 <hernestbill@gmail.com>
2023-10-26 15:39:26 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,19 +6,18 @@
  * @bit: a string that takes a binary number
  * return: numbers that are converted
  */
-unsigned int binary_to_uint(const char * b)
+unsigned int binary_to_uint(const char *b)
 {
-	int alx;
 	unsigned int i = 0;
 
 	if (!b)
 		return (0);
-	for(alx = 0; b[alx]; alx++)
+	while(*b)
 	{
-		if (b[alx] < '0' || b[alx] > '1')
+		if (*b != '0' && *b != '1')
 			return (0);
-		(i = 2 * i + (b[alx]));
+		(num = num *2 + (b++ - '0');
 	}
-	return (i);
+	return (num);
 }
 
