95bb6adff0acbe5d27d6c04e1f0f385dda6b7bcb
billernest128 <hernestbill@gmail.com>
2023-10-26 14:49:37 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,12 +4,12 @@
  * print binary equivalent of a decimal number
  * @n: the decimal number to be printed in binary
  */
-int _putchar;
+int putchar;
 void print_binary(unsigned long int n)
 {
 	int a;
 	unsigned long int new;
-       	int b = 0;
+	int b = 0;
 
 	for (a = 63; a >= 0; a--)
 	{
