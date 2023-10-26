34aedc8a41c881ebf7e2c06254039615973afa28
billernest128 <hernestbill@gmail.com>
2023-10-26 15:17:20 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,15 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * set_bit - the given index sets a bit 1
- * @n: its nothing but a pointer
- * @index: an index that set a bit
- * return: 1 success, -1 fail
+ * set the value of a bit to 1 at a given index.
+ * @n: a pointer to the unsigned long int to modify
+ * @index: the index of the bit to set (starting from 0)
+ * Return: 1 if it worked, -1 if an error occurred
  */
-int set_bit (unsigned long int *n, unsigned int index)
-{
-	if (index > 63)
-		return (-1);
-	*n = ((1UL << index)) 1 *n);
-		return (1);
+int set_bit(unsigned long int *n, unsigned int index) {
+    if (index >= 64) {
+        // Index is out of range, return -1 to indicate an error
+        return -1;
+    }
+
+    unsigned long int mask = 1UL << index;  // Create a mask with a 1 at the specified index
+    *n = *n | mask;  // Use bitwise OR to set the bit to 1
+
+    return 1;
 }
+
+int main() {
+    unsigned long int num = 42;  // Replace with your number
+    unsigned int bitIndex = 3;  // Replace with the index you want to set
+
+    int result = set_bit(&num, bitIndex);
+
+    if (result == 1) {
+        printf("Bit at index %u is set to 1. New value: %lu\n", bitIndex, num);
+    } else {
+        printf("Error: Index out of range\n");
+    }
+
+    return 0;
+}
+
